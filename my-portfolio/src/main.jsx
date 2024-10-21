d8a08ba66ad49a67ccd4c6d5aefa4ea5ef6d323f
andrew-brown <andrewmbrown97@gmail.com>
2024-10-21 15:56:25 -0400

    test html

--- my-portfolio/src/main.jsx
+++ my-portfolio/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './Footer.jsx' // is this useful 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
