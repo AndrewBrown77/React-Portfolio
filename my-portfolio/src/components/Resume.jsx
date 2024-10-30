fbf8638a5aba67887d52feaf4ddaa892d246ee69
andrew-brown <andrewmbrown97@gmail.com>
2024-10-29 21:59:34 -0400

    adding react bootstrap

--- my-portfolio/src/components/Resume.jsx
+++ my-portfolio/src/components/Resume.jsx
@@ -5,14 +5,12 @@ const Resume = () => {
       >
           <div className="pb-8">
             <p className="resume">
-              About
+              Resume
             </p>
           </div>
   
           <p className="resume">
-          I am a web developer, working in both, backend and frontend programming. Excited for improving my skills and learning new technologies.I am open to learn and work with any web technology ands.
-          I value minimalistic designs, thoughtful branding of the content, and customer relatable experience.<br /> <br />
-          I enjoy creating or redesigning a distinct identity for a product or service, get more traffic from search engine and social platforms.
+          I am a web developer, working in both, backend and frontend programming. Excited for impro
           </p>
         </div>
     );
